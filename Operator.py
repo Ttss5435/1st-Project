f39881f713a31bbcbfc233398ee7f12a555b7536
Trisha Singh <trishasingh3131@gmail.com>
2025-04-30 10:25:34 +0530

    Pyhton code using arithmatic operator

--- Operator.py
+++ Operator.py
@@ -1,102 +1,10 @@
-'''#Arithmatic Operator 
+#Arithmatic Operator 
 a=12
 b=55
 print(a-b)
 print(a+b)
 print(a*b)
 print(a/b)
-print(b//a)'''
+print(b//a)
 
-#Comparison Operator
-
-'''c=95
-d=9
-print(c==d)
-print(c!=d)
-print(c<d)
-print(c>d)
-print(c*d)'''
-
-#logical Operator 
-'''a=5
-print(a>2 and a<6)
-print(a>2 or a<6)
-print(not(a>2 and a<6))'''
-
-#Bit-wise Operator
-'''a=6
-b=4
-print(a&b)
-print(a|b)
-print(a^b)'''
-
-#Condition Operator
-
-'''a=int(input("enter your age"))
-b=22
-if a>=18:
-    print("this is eligible for voting")
-else:
-    print("not eligible for voting")
-    a=10
-    b=20
-    if a>b:
-        print ("a is greator than b")
-
-Days=int (input("enter number:"))
-match Days:
-    case 1:
-        print("Sunday")
-    case 2:
-        print("Monday")
-    case 3:
-        print("Tuesday")
-    case 4:
-        print("Wednesday")
-    case 5:
-        print("Thursday")
-    case 6:
-        print("Friday")
-    case 7:
-        print("Saturday")   
-
-#loops
-# for loop 
-from datetime import date
-
-
-for p in range (1,4):
-    print("data")5
-    print("Science")   
-print("Bye")
-
-A="Trisha Singh"
-for P in A:
-    print(P)  '''
-
-
-
-# start = 1
-# end = 10
-
-# print("Odd numbers between", start, "and", end, ":")
-# for number in range(start, end + 1):
-#     if num % 2 != 0: 
-#         print(number)
-
-
-
-# start = 1
-# end = 10
-
-# print("Even numbers between", start, "and", end, ":")
-# for number in range(start, end + 1):
-#     if number % 2 == 0:
-#         print(number)
-
-# for h in range(2,10,2):
-#     print(h)
-
-# for h in range(1,51):
-#     if h % 2==0:
-#         print(h)
+ 
